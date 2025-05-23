705203a82dbb0d3cd19c66782ee9a25bbc90fc9b
Billy G. Allie <48361524+bgallie@users.noreply.github.com>
2025-05-23 17:05:26 -0400

    Updated dependencies including security patches.

--- cmd/root.go
+++ cmd/root.go
@@ -184,7 +184,7 @@ func initEngine(args []string) {
 	}
 
 	// Initialize the tntengine with the secret key and the named proforma file.
-	tntMachine.Init([]byte(secret), proFormaFileName)
+	tntMachine.Init([]byte(secret))
 	// Set the engine type and build the cipher machine.
 	tntMachine.SetEngineType("E")
 	tntMachine.BuildCipherMachine()
@@ -243,7 +243,7 @@ func shutdownTntMachine() {
 	cMap[mKey] = tntMachine.Index()
 	checkError(writeCounterFile(cMap))
 
-	var blk tntengine.CypherBlock
+	var blk tntengine.CipherBlock
 	tntMachine.Left() <- blk
 	<-tntMachine.Right()
 }
